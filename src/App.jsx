e57f793456ca7671b3a9afcd676f0ed74cef5d60
Jayant Suri <reetusuri1983@gmail.com>
2025-07-07 12:06:20 -0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const GameIntro = () => {
       </video>
 
       <div className="a">
-        <img src="/pic.webp" alt="" width={250}/><br /><br /><br />
-        <h1>My self Infinty Jayant</h1><br />
-        <h2>I am on the path to be a coder</h2><br />
+        <img src="/pic.webp" alt="" width={200} className='pic'/><br /><br /><br />
+        <h1 className='jayant'>My self Infinty Jayant</h1><br />
+        <h2 className='code'>I am on the path to be a coder</h2><br />
         <h3>My Masters web:</h3>
         <a href="https://www.bing.com/ck/a?!&&p=4c083134e018d05a10003d236ad3a8b3dcbeb495854b6ad58cb2a7beddb159f4JmltdHM9MTc1MTI0MTYwMA&ptn=3&ver=2&hsh=4&fclid=3afaf54f-da04-6652-0fa7-e0d1db77679a&psq=deep+data+india+research+center&u=a1aHR0cHM6Ly93d3cuZGVlcGRhdGFpbmRpYS5jb20v&ntb=1">Deep Data India Rearch Center</a><br />
         <h3>My Discord Server:</h3>
